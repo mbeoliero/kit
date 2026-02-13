7cd8cfc68c89a60a6447b43e960867ee1b9f7a3d
Jaken <mbeoliero@gmail.com>
2026-02-13 20:44:37 +0800

    feat: update

--- connector/config.go
+++ connector/config.go
@@ -5,7 +5,7 @@ type MysqlConfig struct {
 	WritePath       string `json:"write_path" yaml:"write_path" mapstructure:"write_path"` // 服务器地址:端口
 	ReadPath        string `json:"read_path" yaml:"read_path" mapstructure:"read_path"`
 	Config          string `json:"config" yaml:"config" mapstructure:"config"`                                  // 高级配置
-	Dbname          string `json:"db_name" yaml:"db_name" mapstructure:"dbname"`                                // 数据库名
+	Dbname          string `json:"db_name" yaml:"db_name" mapstructure:"db_name"`                               // 数据库名
 	Username        string `json:"username" yaml:"username" mapstructure:"username"`                            // 数据库用户名
 	Password        string `json:"password" yaml:"password" mapstructure:"password"`                            // 数据库密码
 	MaxIdleConns    int    `json:"max_idle_conns" yaml:"max_idle_conns" mapstructure:"max_idle_conns"`          // 空闲中的最大连接数
